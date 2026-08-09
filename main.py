9771256743005511dfe4f5f9a5a90947e4fe824f
Saad <mskscotland@gmail.com>
2026-08-09 12:18:09 +0100

    Refactor window name and enhance comments
    Updated window naming and added comments for clarity.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np 
+import numpy as np
 import math
 import random
 
@@ -8,13 +8,18 @@ y = 1920
 num_particles = 600
 
 window = np.zeros((x, y, 3))
-cv2.namedWindow(window, cv2.WND_PROP_FULLSCREEN)
+cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-# Random particle ex-y position generated
-particle_pos_x = [random.randint(5, x-5) for _ in range(num_particles)]
+# Random particle ex-y position generated 
+# # Making an array, with each element a particle's position
+particle_pos_x = [random.randint(5, x-5) for _ in range(num_particles)] # This is within boundaries of the screen#
 particle_pos_y = [random.randint(5, y-5) for _ in range(num_particles)]
 
+
 while True:
+    # Loop through every particle and draw it as a circle on the screen
+    for idx in range(num_particles):
+        cv2.circle(window, (int(particle_pos_y[idx]), int(particle_pos_x[idx])), 3, (255, 0, 0), -1)
     cv2.imshow("window", window)
     cv2.waitKey(1)
