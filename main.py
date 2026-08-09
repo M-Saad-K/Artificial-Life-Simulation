180a918f1ce02600e307f6a30c972340112eba7e
Saad <mskscotland@gmail.com>
2026-08-09 12:13:07 +0100

    Typo with X

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cv2.namedWindow(window, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 # Random particle ex-y position generated
-particle_pos_x = [random.randint(5, X-5) for _ in range(num_particles)]
+particle_pos_x = [random.randint(5, x-5) for _ in range(num_particles)] 
 
 while True:
     cv2.imshow("window", window)
