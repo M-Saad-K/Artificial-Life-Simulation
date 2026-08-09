729c5440c66fad4689786f5b7e3253607015028c
Saad <mskscotland@gmail.com>
2026-08-09 12:11:58 +0100

    Generate random x positions for particles
    Added random particle x position generation for simulation.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ window = np.zeros((x, y, 3))
 cv2.namedWindow(window, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+# Random particle ex-y position generated
+particle_pos_x = [random.randint(5, X-5) for _ in range(num_particles)]
+
 while True:
     cv2.imshow("window", window)
     cv2.waitKey(1)
